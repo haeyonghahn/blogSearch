9b86c5bd1064bb75fcf7c12f3a61d0f195ce57d6
haeyonghahn <yong80211@gmail.com>
2023-03-22 23:12:22 +0900

    fix: 블로그 검색 파라미터 valid 수정

--- src/main/java/com/example/blog/dto/BlogRequestDto.java
+++ src/main/java/com/example/blog/dto/BlogRequestDto.java
@@ -15,6 +15,6 @@ public class BlogRequestDto {
     private String sort;
     @Range(min = 1, max = 50, message = "페이지 번호는 1~50 사이의 값이어야 합니다.")
     private Integer page;
-    @Range(min = 1, max = 80, message = "한 페이지에 보여질 문서 수는 1~80 사이의 값이어야 합니다.")
+    @Range(min = 1, max = 50, message = "한 페이지에 보여질 문서 수는 1~50 사이의 값이어야 합니다.")
     private Integer size;
 }
